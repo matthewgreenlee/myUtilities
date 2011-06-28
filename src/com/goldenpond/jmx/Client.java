5a6ed9e4bd81578c0ad1461bd9ef9290f0e990e2
matthew.green.lee@gmail.com <matthew.green.lee@gmail.com@feb33c11-865d-c2a0-12bd-e852ca516405>
2011-06-28 06:23:50 +0000

    incremental version

--- src/com/goldenpond/jmx/Client.java
+++ src/com/goldenpond/jmx/Client.java
@@ -2,13 +2,28 @@ package com.goldenpond.jmx;
 
 import java.io.IOException;
 
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
 import com.goldenpond.utils.Print;
 
 public class Client {
 
-	private static JMXServiceURL extractJMXServiceURL(int pid) throws Exception {
+	private JMXConnector jmxc;
+
+	public void connect(String host, int port) throws Exception {
+		JMXServiceURL url = new JMXServiceURL("rmi", host, port, "/jndi/rmi://" + host + ":" + port + "/jmxrmi");
+		jmxc = JMXConnectorFactory.connect(url);
+		Print.ln("Get the MBeanServerConnection");
+	}
+
+	public void disconnect() throws Exception {
+		Print.ln("Close the MBeanServerConnection");
+		jmxc.close();
+	}
+
+	public JMXServiceURL extractJMXServiceURL(int pid) throws Exception {
 		String serviceURL = null;
 		try {
 			serviceURL = sun.management.ConnectorAddressLink.importFrom(pid);
@@ -21,4 +36,11 @@ public class Client {
 		else
 			return new JMXServiceURL(serviceURL);
 	}
+
+	public static void main(String[] args) throws Exception {
+		Client client = new Client();
+		client.connect("p607023d", 9999);
+		client.disconnect();
+	}
+
 }
