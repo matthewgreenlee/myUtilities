734ab933cae54140773391a62f43b3d8b0b419c1
matthew.green.lee@gmail.com <matthew.green.lee@gmail.com@feb33c11-865d-c2a0-12bd-e852ca516405>
2011-07-13 12:34:54 +0000

    add BubbleSort and its test

---
+++ src/com/goldenpond/algorithm/BubbleSort.java
@@ -0,0 +1,17 @@
+package com.goldenpond.algorithm;
+
+public class BubbleSort {
+
+	public int[] sort(int[] a) {
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = 0; j < a.length - 1 - i; j++) {
+				if (a[j] > a[j+1]) {
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+				}
+			}
+		}
+		return a;
+	}
+}
